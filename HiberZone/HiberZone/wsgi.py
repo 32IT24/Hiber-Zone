1d170e07ba031b142129a2cac38af56188a59630
32IT24 <121421408032@josephscollege.ac.in>
2024-04-01 01:57:18 +0530

    wsgi changed

--- HiberZone/HiberZone/wsgi.py
+++ HiberZone/HiberZone/wsgi.py
@@ -8,7 +8,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+# add your project directory to the sys.path
+project_home = '/home/32IT24/HiberZone'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'HiberZone.settings'
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HiberZone.settings')
